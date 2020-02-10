b7af3d3beb9ce3758cdd786343cf090515de0613
folukeOpenuni <folukeagbede@googlemail.com>
2020-02-10 15:36:54 +0000

    Update DB.php

--- CW-InternetTechnology/model/DB.php
+++ CW-InternetTechnology/model/DB.php
@@ -21,7 +21,7 @@ class DB {
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=faagbede', 'faagbede', '177Middlesex', $pdo_options);
+        self::$instance = new PDO('mysql:host=localhost;dbname=faagbede', 'faagbede', '******', $pdo_options);
       }
       return self::$instance;
     }
